54373b9fafb47c9bd56f5af3abb1e6995a9d2acf
Chongyun Lee <uchkks@protonmail.com>
2023-10-04 14:01:58 +0800

    libandroid: do not extract source when cross-compile

--- packages/libandroid/build.sh
+++ packages/libandroid/build.sh
@@ -10,9 +10,26 @@ TERMUX_PKG_SRCURL=https://dl.google.com/android/repository/android-ndk-r${TERMUX
 TERMUX_PKG_SHA256=1505c2297a5b7a04ed20b5d44da5665e91bac2b7c0fbcd3ae99b6ccc3a61289a
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_get_source() {
+	if [ "$TERMUX_ON_DEVICE_BUILD" = true ]; then
+		termux_download_src_archive
+		cd $TERMUX_PKG_TMPDIR
+		termux_extract_src_archive
+	fi
+	mkdir -p $TERMUX_PKG_SRCDIR
+}
+
 termux_step_post_make_install() {
+	local _ndk_prefix
+
+	if [ "$TERMUX_ON_DEVICE_BUILD" = true ]; then
+		_ndk_prefix="$TERMUX_PKG_SRCDIR"
+	else
+		_ndk_prefix="$NDK"
+	fi
+
 	install -v -Dm644 \
-		"toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/libandroid.so" \
+		"$_ndk_prefix/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/libandroid.so" \
 		"$TERMUX_PREFIX/lib/libandroid.so"
 }
 
