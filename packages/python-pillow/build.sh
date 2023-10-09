b5c60cbe52176c42a921a9f27f97ffeeeaa68d8d
Lucy Phipps <landfillbaby69@gmail.com>
2023-10-10 00:37:55 +0100

    revbump python-pillow

--- packages/python-pillow/build.sh
+++ packages/python-pillow/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Python Imaging Library"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=10.0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=5df55f87434f1b42d9ebe4247ed50a0f0742cd1ad5be2820e3d1b1f4b4bc696f
 TERMUX_PKG_SRCURL=https://github.com/python-pillow/Pillow/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="freetype, libimagequant, libjpeg-turbo, libraqm, libtiff, libwebp, libxcb, littlecms, openjpeg, python, zlib"
